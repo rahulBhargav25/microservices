d502659c2746ca535c2329d62ed7164edb45d96d
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-26 00:38:03 +0530

    rating microservice

---
+++ src/main/java/com/example/ratingsdataservice/model/UserRating.java
@@ -0,0 +1,24 @@
+package com.example.ratingsdataservice.model;
+
+import java.util.List;
+
+public class UserRating {
+    private List<Rating> userRating;
+
+
+    public UserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+
+    public UserRating() {
+
+    }
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+}
