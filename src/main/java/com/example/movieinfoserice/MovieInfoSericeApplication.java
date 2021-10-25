42ddb20dd17bd9f4fff06790f2bbcc97bc1b4b16
Rahul Bhargav <rahulbhargav26@gmail.com>
2021-10-26 00:35:44 +0530

    Movie info of microservice

---
+++ src/main/java/com/example/movieinfoserice/MovieInfoSericeApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieinfoserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoSericeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieInfoSericeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movieinfoserice/MovieInfoSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieinfoserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoSericeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
